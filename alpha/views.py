460f023fc0a69a11d67ad670514073fc55301428
umar <kimumar55@gmail.com>
2021-11-03 09:06:51 +0100

    jj

--- alpha/views.py
+++ alpha/views.py
@@ -1,14 +1,64 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        send_mail(
+            name,
+            message,
+            'kimumar55@gmail.com',
+            ['info@noziroh.com.ng'],
+            fail_silently=False)
+    
     return render(request, 'index.html')
 
 def about(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        send_mail(
+            name,
+            message,
+            'kimumar55@gmail.com',
+            ['info@noziroh.com.ng'],
+            fail_silently=False)
+
     return render(request, 'about.html')
 
 def services(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        send_mail(
+            name,
+            message,
+            'kimumar55@gmail.com',
+            ['info@noziroh.com.ng'],
+            fail_silently=False)
+    
     return render(request, 'services.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        send_mail(
+            name,
+            message,
+            'kimumar55@gmail.com',
+            ['info@noziroh.com.ng'],
+            fail_silently=False)
+    
     return render(request, 'contact.html')
--- noziroh/settings.py
+++ noziroh/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import environ
+#Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i+rtpl%snb*_x5ec&og07s+w6t&z+1@4ue7s*nd^&yl!hm!$)#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +128,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#Email Settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = env('email_user')
+EMAIL_HOST_PASSWORD = env('email_pass')
+EMAIL_USE_TLS = True
